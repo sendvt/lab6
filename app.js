539fe8a608c0a5f11346cf8da7d2742edba1fd41
vagrant <vagrant@precise32.(none)>
2015-02-13 18:13:45 +0000

    Lab 6 finished/

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var index = require('./routes/index');
 var project = require('./routes/project');
 // Example route
 // var user = require('./routes/user');
-
+var palette = require('./routes/palette');
 var app = express();
 
 // all environments
@@ -38,9 +38,11 @@ if ('development' == app.get('env')) {
 // Add routes here
 app.get('/', index.view);
 app.get('/project/:id', project.projectInfo);
+app.get('/palette', palette.randomPalette);
 // Example route
 // app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
